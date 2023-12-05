df720777e61747b4bee3ff44dd45b8c6aedd7b75
CristianGodoy81 <cristiangodoy81@gmail.com>
2023-12-05 20:57:29 -0300

    Jornada del 5/12/23

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,23 +4,32 @@ const fs = require('fs'); // Cristian
 const productsFilePath = path.join(__dirname, '../database/products.json'); // Cristian
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); // Cristian
 
-
-const controller = { // Cristian
-    create: (req, res)=>{
+const controller = { 
+    create: (req, res)=>{ // Cristian
         res.render(path.resolve(__dirname,'../views/products/productCreate.ejs'))
     },
-    createOk: (req, res)=>{
-        res.send("Todo marcha bien Milhouse");
+    createOk: (req, res)=>{ // Cristian
+        const newProduct = {id: products.length+1, ...(req.body)};
+        products.push(newProduct);
+        //console.log(products);
+        res.send("Producto creado correctamente");
     },
-    edit: (req, res)=>{
+    edit: (req, res)=>{ // Cristian
         for(let i=0; i<products.length; i++){
 			if(products[i].id == (req.params).id){
                 res.render(path.resolve(__dirname,'../views/products/productEdit.ejs'), {product: products[i]})
 			}
 		}
     },
-    editOk: (req, res)=>{
-        res.send("Todo marcha bien Milhouse");
+    editOk: (req, res)=>{ // Cristian
+        const productEdit = {id: (req.params).id, ...(req.body)};
+        for(let i=0; i<products.length; i++){
+			if(products[i].id == (req.params).id){
+                products[i] = productEdit;
+			}
+		}
+        //console.log(products);
+        res.send("Producto editado correctamente");
     }
 };
 
