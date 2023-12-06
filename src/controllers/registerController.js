4f95064b003dd4a0e0e71cd2478f902954f85dda
GustavoDanielMoyaVera <GustavoDanielMoyaVera@gmail.com>
2023-12-06 12:14:39 -0300

    terminé el controller del metodo put de REGISTER para pushear los datos ingresados en la base de datos, falta agregar los nuevos campos al formulario

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('node:path');
+const fs = require("fs");
 
 
 const controller = {
@@ -8,16 +9,26 @@ const controller = {
         res.render(path.resolve(__dirname, '../views/users/register.ejs'));
     },
     create: (req, res) => {
-        res.send(`Controller funcionando, falta enviar info al JSON`)
-        console.log(req.body);
-        // let usuario = {
-        //     nombreYApellido: req.body.nombreYApellido,
-        //     correoElectronico: req.body.correoElectronico,
-        //     telefono: req.body.telefono,
-        //     contrasenia: req.body.contrasenia,
-        //     contrasenia2: req.body.contrasenia2
-        // };
-        // res.redirect("/");
+        let usuario = {
+            nombreYApellido: req.body.nombreYApellido,
+            correoElectronico: req.body.correoElectronico,
+            telefono: req.body.telefono,
+            contrasenia: req.body.contrasenia,
+            contrasenia2: req.body.contrasenia2
+        };
+        let archivoUser = fs.readFileSync("src/database/user.json", { encoding: "utf-8" })
+        let usuarios;
+        if (archivoUser == "") {
+            usuarios = [];
+        } else {
+            usuarios = JSON.parse(archivoUser);
+        };
+
+        usuarios.push(usuario);
+
+        usuariosJSON = JSON.stringify(usuarios);
+        fs.writeFileSync("src/database/user.json", usuariosJSON);
+        res.redirect("/");
     }
 };
 
