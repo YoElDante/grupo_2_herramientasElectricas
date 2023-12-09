97d4e196b70e2d4cff434761a20862ba664a4c47
CristianGodoy81 <cristiangodoy81@gmail.com>
2023-12-09 15:35:43 -0300

    Desde el formulario de creacion se pueden agregar productos y ver impactados en el archivo JSON para tal fin

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,16 +2,20 @@ const express = require('express'); // Cristian
 const path = require('path'); // Cristian
 const fs = require('fs'); // Cristian
 const productsFilePath = path.join(__dirname, '../database/products.json'); // Cristian
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); // Cristian
+const productsJSON = fs.readFileSync(productsFilePath, 'utf-8'); // Cristian
+const products = JSON.parse(productsJSON); // Cristian
 
 const controller = { 
     create: (req, res)=>{ // Cristian
         res.render(path.resolve(__dirname,'../views/products/productCreate.ejs'))
     },
     createOk: (req, res)=>{ // Cristian
+        const productsJSON = fs.readFileSync(productsFilePath, 'utf-8'); // Lo repito para tenerlo actualizado (5)
+        const products = JSON.parse(productsJSON); // Lo repito para tenerlo actualizado (6)
         const newProduct = {id: products.length+1, ...(req.body)};
         products.push(newProduct);
-        //console.log(products);
+        const newProductsJSON = JSON.stringify(products);
+        fs.writeFileSync(productsFilePath,newProductsJSON);
         res.send("Producto creado correctamente");
     },
     edit: (req, res)=>{ // Cristian
@@ -21,14 +25,7 @@ const controller = {
 			}
 		}
     },
-    editOk: (req, res)=>{ // Cristian
-        const productEdit = {id: (req.params).id, ...(req.body)};
-        for(let i=0; i<products.length; i++){
-			if(products[i].id == (req.params).id){
-                products[i] = productEdit;
-			}
-		}
-        //console.log(products);
+    editOk: (req, res)=>{
         res.send("Producto editado correctamente");
     }
 };
