f585fee21298b37fd80e268cf33a02b3ce91b67c
YoElDante <vivayodante@gmail.com>
2023-12-13 09:11:20 -0300

    Se hicieron los partials dinamicos de  productCard y productCardSale y se los incorporo al home

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const path = require('node:path');
+const express = require('express'); 
+const path = require('path'); 
+const fs = require('fs'); 
 
+const productsFilePath = path.join(__dirname, '../database/products.json'); 
+const productsJSON = fs.readFileSync(productsFilePath, 'utf-8'); 
+const products = JSON.parse(productsJSON); 
 
 const controller = {
     
     index: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/home.ejs'));
+        res.render(path.resolve(__dirname, '../views/home.ejs'), {products});
     },
 
 };
