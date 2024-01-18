56343ab5f79b546edea14940af49f08e4eda0156
Florencia Tula <48216488+flops1986@users.noreply.github.com>
2024-01-18 20:24:15 +0000

    sume las rutas - flor

--- src/routers/products.js
+++ src/routers/products.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const path = require('path');
 const productsController = require('../controllers/productsController.js');
 const imgUpload = require('../middlewares/multerMiddleware.js')
+const logined = require ('../middlewares/loginedMiddleware.js');
 
 // rutas
+
+router.get('/create', logined, productsController.create);
+router.post('/create', imgUpload.array(image), productsController.createOk);
+
+router.get('/edit/:id', logined, productsController.edit);
+router.put('edit/:id', imgUpload.array(image), productsController.editOk);
+
+router.delete('/:id', logined, productsController.delete); 
 
 router.get('/create', productsController.create); // Cristian
 router.post('/create', imgUpload.array('image'), productsController.createOk); // Cristian
@@ -16,7 +25,7 @@ router.delete('/:id', productsController.delete);
 
 router.get('/detail/:id?', productsController.detail);
 
-router.get('/cart', productsController.cart);
+router.get('/cart', logined, productsController.cart);
 
 module.exports = router;
 
