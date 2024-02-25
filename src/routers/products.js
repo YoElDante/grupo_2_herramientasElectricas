78d495b5588baeec02eacbe2d8def406a3557530
CristianGodoy81 <cristiangodoy81@gmail.com>
2024-02-25 15:59:30 -0300

    Pequeña correccion en el parametro de imgUpload

--- src/routers/products.js
+++ src/routers/products.js
@@ -9,22 +9,22 @@ const logined = require ('../middlewares/loginedMiddleware.js');
 router.get('/',productsController.index);
 
 router.get('/create', logined, productsController.create);
-// router.post('/create', imgUpload.array(image), productsController.createOk);
+router.post('/create', imgUpload.array('image'), productsController.createOk); // Cristian: 20-02-24 Le puse comillas a (image).
 
 router.get('/edit/:id', logined, productsController.edit);
-// router.put('edit/:id', imgUpload.array(image), productsController.editOk);
+router.put('/edit/:id', imgUpload.array('image'), productsController.editOk); // Cristian: 20-02-24 Le puse comillas a (image).
 
-router.delete('/:id', logined, productsController.delete); 
+router.delete('/:id', logined, productsController.delete);
 
-router.get('/create', productsController.create); // Cristian
-router.post('/create', imgUpload.array('image'), productsController.createOk); // Cristian
+//router.get('/create', productsController.create); // Cristian: 20-02-24 Esta linea no deberia existir, por estar diplicada. Ver linea 11 (router.get('/create', logined, productsController.create);).
+//router.post('/create', imgUpload.array('image'), productsController.createOk); // Cristian: 20-02-24 Esta linea no deberia existir, por estar diplicada. Ver linea 12 (router.post('/create', imgUpload.array(image), productsController.createOk);).
 
-router.get('/edit/:id', productsController.edit); // Cristian
-router.put('/edit/:id', imgUpload.array('image'), productsController.editOk); // Cristian
+//router.get('/edit/:id', productsController.edit); // Cristian: 20-02-24 Esta linea no deberia existir, por estar diplicada. Ver linea 14 (router.get('/edit/:id', logined, productsController.edit);).
+//router.put('/edit/:id', imgUpload.array('image'), productsController.editOk); // Cristian: 20-02-24 Esta linea no deberia existir, por estar diplicada. Ver linea 15 (router.put('edit/:id', imgUpload.array(image), productsController.editOk);).
 
-router.delete('/:id', productsController.delete);
+//router.delete('/:id', productsController.delete); // Cristian: 20-02-24 Esta linea no deberia existir, por estar diplicada. Ver linea 17 (router.delete('/:id', logined, productsController.delete);).
 
-router.get('/detail/:id?', productsController.detail);
+router.get('/detail/:id', productsController.detail); // Cristian: 20-02-24 Borre un signo de exclamacion ('/detail/:id?').
 
 router.get('/cart', logined, productsController.cart);
 
