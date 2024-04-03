21257075bb291c69bb8049555aca34e2e3209ea2
CristianGodoy81 <cristiangodoy81@gmail.com>
2024-04-03 01:23:02 -0300

    Implementaciones para ver en el home, las tarjetas de novedades de productos

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,29 @@
 const express = require('express'); 
 const path = require('path'); 
 const products = require('../database/services/productsdataAccessService.js');
+const db = require('../database/models');
 
 const controller = {
     index: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/home.ejs'), {products});
+        db.Product.findAll({
+            include: [
+                {association: "productBrand"},
+                {association: "productImages"}
+            ]
+        })
+        .then((products)=>{
+            res.render(path.resolve(__dirname, '../views/home.ejs'),
+                {
+                    novedad1: products[products.length - 1],
+                    novedad2: products[products.length - 2]
+                }
+            );
+        })
     },
+    /* Viejo index
+    index: (req, res) => {
+        res.render(path.resolve(__dirname, '../views/home.ejs'), {products});
+    }, */
     dashboard: (req, res) => {
         res.render(path.resolve(__dirname, '../views/dashboard.ejs'));
     }
